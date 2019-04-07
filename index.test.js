5c9bcb4cef42cf32b527a0020852ee7714dee5ee
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-04-07 12:55:37 +0200

    Refactor, group tests for then()

--- index.test.js
+++ index.test.js
@@ -15,84 +15,86 @@ describe("slow-promise", () => {
     const processNextPromiseChain = async () =>
         new Promise(f => setImmediate(f));
 
-    it("does not fulfill if timeout hasn't occurred yet", async () => {
-        const p = new SlowPromise(f => f());
-        p.then(spy);
+    describe(".then()", () => {
+        it("does not fulfill if timeout hasn't occurred yet", async () => {
+            const p = new SlowPromise(f => f());
+            p.then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(999);
-        await processNextPromiseChain();
-        expect(spy).not.toHaveBeenCalled();
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(999);
+            await processNextPromiseChain();
+            expect(spy).not.toHaveBeenCalled();
+        });
 
-    it("calls a callback after timeout has occurred", async () => {
-        const p = new SlowPromise(f => f());
-        p.then(spy);
+        it("calls a callback after timeout has occurred", async () => {
+            const p = new SlowPromise(f => f());
+            p.then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        expect(spy).toHaveBeenCalled();
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            expect(spy).toHaveBeenCalled();
+        });
 
-    it("calls a callback with value", async () => {
-        const p = new SlowPromise(f => f(42));
-        p.then(spy);
+        it("calls a callback with value", async () => {
+            const p = new SlowPromise(f => f(42));
+            p.then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        expect(spy).toHaveBeenCalledWith(42);
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            expect(spy).toHaveBeenCalledWith(42);
+        });
 
-    it("does not call callback if not fulfilled", async () => {
-        const p = new SlowPromise(() => {});
-        p.then(spy);
+        it("does not call callback if not fulfilled", async () => {
+            const p = new SlowPromise(() => {});
+            p.then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        expect(spy).not.toHaveBeenCalled();
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            expect(spy).not.toHaveBeenCalled();
+        });
 
-    it("does not call the next leg of the promise chain if the timeout hasn't occurred yet", async () => {
-        const p = new SlowPromise(f => f());
-        p.then(() => {}).then(spy);
+        it("does not call the next leg of the promise chain if the timeout hasn't occurred yet", async () => {
+            const p = new SlowPromise(f => f());
+            p.then(() => {}).then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        jasmine.clock().tick(999);
-        await processNextPromiseChain();
-        expect(spy).not.toHaveBeenCalled();
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            jasmine.clock().tick(999);
+            await processNextPromiseChain();
+            expect(spy).not.toHaveBeenCalled();
+        });
 
-    it("calls the next leg of the promise chain if the timeout has occurred", async () => {
-        const p = new SlowPromise(f => f());
-        p.then(() => {}).then(spy);
+        it("calls the next leg of the promise chain if the timeout has occurred", async () => {
+            const p = new SlowPromise(f => f());
+            p.then(() => {}).then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        expect(spy).toHaveBeenCalled();
-    });
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            expect(spy).toHaveBeenCalled();
+        });
 
-    it("doesn't call the third leg if the timeout hasn't occurred yet", async () => {
-        const p = new SlowPromise(f => f());
-        p.then(() => {})
-            .then(() => {})
-            .then(spy);
+        it("doesn't call the third leg if the timeout hasn't occurred yet", async () => {
+            const p = new SlowPromise(f => f());
+            p.then(() => {})
+                .then(() => {})
+                .then(spy);
 
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        jasmine.clock().tick(1000);
-        await processNextPromiseChain();
-        jasmine.clock().tick(999);
-        await processNextPromiseChain();
-        expect(spy).not.toHaveBeenCalled();
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            jasmine.clock().tick(1000);
+            await processNextPromiseChain();
+            jasmine.clock().tick(999);
+            await processNextPromiseChain();
+            expect(spy).not.toHaveBeenCalled();
+        });
     });
 
     describe(".resolve()", () => {
